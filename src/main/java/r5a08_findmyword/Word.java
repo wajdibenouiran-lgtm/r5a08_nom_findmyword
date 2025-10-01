554830431244fad539efa0b9ce3d51a23b81d9be
wajdibenouiran-lgtm <wajdi.benouirane@etudiant-isi.utm.tn>
2025-10-01 11:24:22 +0200

    les 2 premieres questions

---
+++ src/main/java/r5a08_findmyword/Word.java
@@ -0,0 +1,4 @@
+package r5a08_findmyword;
+
+public class Word {
+}
---
+++ src/test/java/r5a08_findmyword/WordTest.java
@@ -0,0 +1,13 @@
+package r5a08_findmyword;
+
+import org.junit.jupiter.api.Test;
+
+
+public class WordTest {
+
+    @Test
+    void should_check_one_incorrect_letter() {
+        Word word = new Word("E");
+
+    }
+}
